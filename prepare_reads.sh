aa3e62b52c9f09384f13c57671de88263d3527fc
Stuart Huntley <stuhlearns@gmail.com>
2023-08-29 13:34:34 -0400

    create prepare_reads.sh script

---
+++ prepare_reads.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This script reads and input fastq file, performs a quality filter
+# on the reads, and splits the file into 12 subread sets.
+
+# It takes one positional argument: read file location
