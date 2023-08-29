17ab66e2ddd513e4c80926d4d8b6140ec07f237f
Stuart Huntley <stuhlearns@gmail.com>
2023-08-29 15:00:32 -0400

    fix filtlong command typo in prepare_reads.sh

--- prepare_reads.sh
+++ prepare_reads.sh
@@ -6,5 +6,5 @@
 # It takes one positional argument: read file location
 # Use filtlong to remove very poor and short (< 1kb) reads from
 # raw read file and save output as reads.fq in working dir.
-filtlong -min_length 1000 --keep_percent 95 input_reads.fastq.gz > reads.fastq
+filtlong --min_length 1000 --keep_percent 95 input_reads.fastq.gz > reads.fastq
 
