1361881688795c684e878cef0a52b0ff81e1a31a
Stuart Huntley <stuhlearns@gmail.com>
2023-08-29 14:57:31 -0400

    add filtlong command to prepare_reads.sh

--- prepare_reads.sh
+++ prepare_reads.sh
@@ -4,3 +4,7 @@
 # on the reads, and splits the file into 12 subread sets.
 
 # It takes one positional argument: read file location
+# Use filtlong to remove very poor and short (< 1kb) reads from
+# raw read file and save output as reads.fq in working dir.
+filtlong -min_length 1000 --keep_percent 95 input_reads.fastq.gz > reads.fastq
+
